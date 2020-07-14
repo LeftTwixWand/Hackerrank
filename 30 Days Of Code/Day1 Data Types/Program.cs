ec54e72d1f809109668f99bda7218455a7707560
Vladyslav <0965142838gvs@gmail.com>
2020-07-14 20:11:20 +0300

    Added projects for days 0, 1, 2, 5

---
+++ 30 Days Of Code/Day1 Data Types/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day1_Data_Types
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+
+            // Declare second integer, double, and String variables.
+            int myInt;
+            double myDouble;
+            string myString;
+
+            // Read and save an integer, double, and String to your variables.
+            int.TryParse(Console.ReadLine(), out myInt);
+            myDouble = double.Parse(Console.ReadLine());
+            myString = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(myInt + i);
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine((myDouble + d).ToString("F1"));
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + myString);
+        }
+    }
+}
---
+++ 30 Days Of Code/Day5 Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5_Loops
+{
+    // https://www.hackerrank.com/challenges/30-loops/problem
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");
+            }
+        }
+    }
+}
