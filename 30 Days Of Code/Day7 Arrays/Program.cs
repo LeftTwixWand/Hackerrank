5e367c75fa3e26e26ad9a74f2e86cfd80d04bba9
Vladyslav <0965142838gvs@gmail.com>
2020-07-17 00:34:59 +0300

    Added Day7 project second commit

---
+++ 30 Days Of Code/Day7 Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Day7_Arrays
+{
+    // https://www.hackerrank.com/challenges/30-arrays
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Array.ForEach(Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp)).Reverse().ToArray(), item => Console.Write($"{item} "));
+        }
+    }
+}
