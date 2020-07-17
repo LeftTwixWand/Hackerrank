159ad426554c26c09f49bdab2f93d445f9acc490
Vladyslav <0965142838gvs@gmail.com>
2020-07-18 02:59:33 +0300

    Added Day8 project

---
+++ 30 Days Of Code/Day8 Dictionaries and Maps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8_Dictionaries_and_Maps
+{
+    // https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            Dictionary<string, string> phones = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                phones[line[0]] = line[1];
+            }
+
+            string name;
+            while ((name = Console.ReadLine()) != null)
+            {
+                if (phones.ContainsKey(name))
+                    Console.WriteLine(name + "=" + phones[name]);
+                else
+                    Console.WriteLine("Not found");
+            }
+        }
+    }
+}
